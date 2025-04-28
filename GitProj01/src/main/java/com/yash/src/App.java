96b3704793c6354feb5b1fbbbdb8631694abc197
Ej532ws <Ej532ws@LAPTOP-BEAST>
2025-04-29 01:10:55 +0530

    Code Of main edited by dev-1

--- GitProj01/src/main/java/com/yash/src/App.java
+++ GitProj01/src/main/java/com/yash/src/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Hehe I completed todays assginment of Git");
     }
 }
