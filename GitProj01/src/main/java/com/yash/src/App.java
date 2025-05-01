9fe6934c4938d3ce217b22c897dd79d7f8380e37
Ej532ws <Ej532ws@LAPTOP-BEAST>
2025-05-01 17:43:27 +0530

    line 11 modified by dev1

--- GitProj01/src/main/java/com/yash/src/App.java
+++ GitProj01/src/main/java/com/yash/src/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! dev modification " );
         System.out.println("Hehe I completed todays assginment of Git");
     }
 }
